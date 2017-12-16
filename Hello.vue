dbc9a088c1f5d5b53801572e38d120b45cda7db6
corocn <crsk.plmzh@gmail.com>
2017-12-16 14:59:53 +0900

    init and add sample

---
+++ Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>{{greeting}} World!</div>
+</template>
+
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        greeting: 'Hello'
+      }
+    },
+    mounted: function() {
+      console.log('mounted!')
+    }
+  }
+</script>
+
+<style>
+  div {
+    color: red;
+  }
+</style>
+
+<style scoped>
+  div {
+    font-size: 32px;
+  }
+</style>
---
+++ README.md
@@ -0,0 +1,18 @@
+# parcel-vue-sfc-sample
+
+Sample code using Parcel and Vue (Single File Component)
+
+```
+yarn install
+yarn parcel index.html
+```
+
+then access http://localhost:1234/
+
+# refs
+
+ - [Parcel](https://parceljs.org/)
+ - [Vue.js](https://jp.vuejs.org/index.html)
+ - [parcel-plugin-vue](https://github.com/lc60005457/parcel-plugin-vue)
+
+Thank you!
